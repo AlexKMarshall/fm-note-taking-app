071267aea6ec5a912df4660ab9644098e0b6eadb
AlexKMarshall <alexmarshall@qogita.com>
2025-07-27 17:39:33 +0100

    ci: try adding react to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
+import react from '@vitejs/plugin-react'
 const dirname =
   typeof __dirname !== 'undefined'
     ? __dirname
@@ -12,7 +13,7 @@ const dirname =
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [tailwindcss(), tsconfigPaths()],
+  plugins: [tailwindcss(), tsconfigPaths(), react()],
   test: {
     projects: [
       {
