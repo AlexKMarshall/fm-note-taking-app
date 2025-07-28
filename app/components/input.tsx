7414d39191b754ed49d1125a1702d4085941feaf
AlexKMarshall <alexmarshall@qogita.com>
2025-07-28 09:04:51 +0100

    style: make input border red when invalid

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -4,7 +4,7 @@ export function Input(props: Omit<ComponentProps<'input'>, 'className'>) {
   return (
     <input
       {...props}
-      className="rounded-lg border border-gray-300 px-4 py-3 hover:bg-gray-50 focus-visible:border-gray-950 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500"
+      className="rounded-lg border border-gray-300 px-4 py-3 invalid:border-red-500 hover:bg-gray-50 focus-visible:border-gray-950 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500 aria-invalid:border-red-500"
     />
   )
 }
