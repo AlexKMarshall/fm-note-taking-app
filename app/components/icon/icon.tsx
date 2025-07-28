e1a025c433249c1f7e258ec5f3fb52bf08c3334d
AlexKMarshall <alexmarshall@qogita.com>
2025-07-28 15:59:43 +0100

    fix: hide icons from screenreaders

--- app/components/icon/icon.tsx
+++ app/components/icon/icon.tsx
@@ -2,6 +2,10 @@ import { type SVGProps } from 'react'
 import { type IconName } from './names'
 import spriteHref from './sprite.svg'
 
+/**
+ * Visual icon component. This is purely decorative, do not use to convey important meaning, always add additional text
+ * where the icon has some meaning.
+ */
 export function Icon({
   name,
   ...props
@@ -9,7 +13,7 @@ export function Icon({
   name: IconName
 }) {
   return (
-    <svg {...props}>
+    <svg aria-hidden="true" role="img" {...props}>
       <use href={`${spriteHref}#${name}`} />
     </svg>
   )
