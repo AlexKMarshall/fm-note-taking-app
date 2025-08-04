969a74abdab8489ad1d03d8b446c60f985262d35
AlexKMarshall <alexmarshall@qogita.com>
2025-08-04 12:32:41 +0200

    test: clean up temporary logging

--- tests/e2e/auth.spec.ts
+++ tests/e2e/auth.spec.ts
@@ -1,28 +1,11 @@
-import { faker } from '@faker-js/faker'
-import { test, expect } from '../playwright-utilities'
-import { users } from '~/database/schema'
+import { expect, test } from '../playwright-utilities'
 
 test.describe('unauthenticated user', () => {
   test.use({ authStatus: 'unauthenticated' })
 
-  test('homepage redirects to login if not authenticated', async ({
-    page,
-    db,
-  }) => {
+  test('homepage redirects to login if not authenticated', async ({ page }) => {
     // TODO explicitly don't have a session cookie (right now we just don't have one by default)
     await page.goto('/')
-
-    // TODO: this is just here to verify we can manually interact with the db during a test
-    // remove it once we've proven it works in ci
-    const [manuallyInsertedTestUser] = await db
-      .insert(users)
-      .values({
-        email: faker.internet.email(),
-        passwordHash: faker.internet.password(),
-      })
-      .returning()
-
-    console.log('manuallyInsertedTestUser', manuallyInsertedTestUser)
 
     await expect(
       page.getByRole('heading', { name: 'Welcome to Note' }),
