142dc9ee67fb4c4bb899a0aac0377653e4ab22c8
AlexKMarshall <alexmarshall@qogita.com>
2025-08-06 08:36:49 +0100

    test: check for tampered session in requireUser

--- app/lib/require-user.server.test.ts
+++ app/lib/require-user.server.test.ts
@@ -34,3 +34,28 @@ test('throws a redirect to login for a missing session', async () => {
     }),
   )
 })
+
+test('throws a redirect to login for a malformed session', async () => {
+  const sessionStorage = createSessionStorage({
+    SESSION_SECRET: 'test',
+    ENVIRONMENT: 'test',
+  })
+  const tamperedSessionStorage = createSessionStorage({
+    SESSION_SECRET: 'tampered',
+    ENVIRONMENT: 'test',
+  })
+  const tamperedSession = await tamperedSessionStorage.getSession()
+  tamperedSession.set('userId', 1)
+
+  const request = new Request('http://localhost/notes', {
+    headers: {
+      Cookie: await tamperedSessionStorage.commitSession(tamperedSession),
+    },
+  })
+
+  await expect(requireUser({ request, sessionStorage })).rejects.toEqual(
+    expect.objectContaining({
+      status: 302,
+    }),
+  )
+})
