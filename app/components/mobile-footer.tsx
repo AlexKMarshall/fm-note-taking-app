de29e9fa9112c9d13e8b76a21056f838c9ffafb6
AlexKMarshall <alexmarshall@qogita.com>
2025-08-09 14:31:13 +0100

    docs: add context for the tailwind arbitrary class

--- app/components/mobile-footer.tsx
+++ app/components/mobile-footer.tsx
@@ -3,7 +3,10 @@ import { Separator } from './separator'
 
 export function MobileFooter() {
   return (
-    <footer className="flex justify-between overflow-x-auto border-t border-gray-200 bg-white px-4 py-3 shadow-[0_-4px_6px_hsl(0deg_0%_94%_/_0.6)] md:px-8 lg:hidden">
+    <footer
+      // Uses a custom shadow because the shadow appears _above_ the footer, this is a bespoke one-off
+      className="flex justify-between overflow-x-auto border-t border-gray-200 bg-white px-4 py-3 shadow-[0_-4px_6px_hsl(0deg_0%_94%_/_0.6)] md:px-8 lg:hidden"
+    >
       <FooterLink to="/notes" label="Home" icon="icon-home" />
       <Separator orientation="vertical" className="max-sm:hidden" />
       <FooterLink to="/notes/search" label="Search" icon="icon-search" />
